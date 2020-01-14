ac4118ce42082f6811e013829e86f78053f28167
utkace <utkarsh@mindship.io>
2020-01-14 23:17:32 +0530

    feature: add ImageKitContext component

---
+++ src/components/IKContext.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: "IKContext",
+
+  inheritAttrs:false,
+
+  provide() {
+    const contextConfigurations = {}
+    Object.assign(contextConfigurations, this.$attrs);
+    return {configurations:contextConfigurations};
+  },
+
+  computed: {
+    attributes() {
+      return this.$attrs
+    }
+  },
+
+   render(h) {
+    return h(
+      "div",
+      {
+        class: { "ik-context": true },
+        attrs: this.attributes
+      },
+      this.$slots.default
+    );
+  }
+};
+</script>
