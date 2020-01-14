acbbaae86b788f1707184e1bfd1c37a4f5febe3e
utkace <utkarsh@mindship.io>
2020-01-14 23:00:01 +0530

    chore: initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential"],
+  rules: {
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  setupFiles: ['<rootDir>/.jest/register-context.js'],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: [
+    "**/tests/**/*.test.(js|jsx|ts|tsx)|**/__snapshots__/*.(js|jsx|ts|tsx)"
+  ],
+  testURL: "http://localhost/"
+};
