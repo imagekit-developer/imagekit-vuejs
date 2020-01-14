a2c6a5558325e2dec93d1348532171899fd3a33b
utkace <utkarsh@mindship.io>
2020-01-14 23:25:39 +0530

    helpers: add upload helper

---
+++ src/helpers/upload.js
@@ -0,0 +1,64 @@
+import ImageKit from 'imagekit-javascript';
+
+export const uploadImage = ({ e, file, fileName, useUniqueFileName, tags, folder, isPrivateFile, customCoordinates, responseFields, publicKey, urlEndpoint, authenticationEndpoint }) => {
+
+  if (!publicKey) {
+    throw new Error("Missing publicKey during initialization");
+  }
+
+  if (!urlEndpoint) {
+    throw new Error("Missing urlEndpoint during initialization");
+  }
+
+  if (!authenticationEndpoint) {
+    throw new Error("Missing authenticationEndpoint during initialization");
+  }
+
+  let onError = (e, err) => {
+    e.insertAdjacentHTML(
+      "afterend",
+      `<div>${err}</div>`
+    );
+  };
+
+  let onSuccess = (e) => {
+    e.insertAdjacentHTML(
+      "afterend",
+      `<div>Image Uploaded</div>`
+    );
+  };
+
+  const ik = new ImageKit({
+    publicKey: publicKey,
+    urlEndpoint: urlEndpoint,
+    authenticationEndpoint: authenticationEndpoint
+  });
+
+  const params = {
+    file: file,
+    fileName: fileName,
+    useUniqueFileName: useUniqueFileName,
+    isPrivateFile: isPrivateFile,
+    folder: folder,
+  }
+  if (tags) {
+    Object.assign(params, { tags: tags });
+  }
+
+  if (customCoordinates) {
+    Object.assign(params, { customCoordinates: customCoordinates });
+  }
+
+  if (responseFields) {
+    Object.assign(params, { responseFields: responseFields });
+  }
+
+  ik.upload(params
+    , function (err, result) {
+      if (err) {
+        onError(e, err);
+      } else {
+        onSuccess(e);
+      }
+    });
+}
