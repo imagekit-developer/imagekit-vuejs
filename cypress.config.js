fc22bae97f596eb83249a4216fa6ac7df074fdbe
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2023-11-23 15:41:14 +0530

    cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,18 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  fixturesFolder: "./cypress/fixtures",
+  screenshotsFolder: "./cypress/screenshots",
+  videosFolder: "./cypress/videos",
   env: {
-    APP_HOST: 'http://localhost:3000/',
+    APP_HOST: "http://localhost:3000/",
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // implement node event listeners here
+      return require("./cypress/plugins/index.js")(on, config);
     },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.js",
   },
-  defaultCommandTimeout: 10000
-})
+});
