cd4214b0cf0ac0c45e82936ffedd6ff8195eaf5e
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2023-11-27 09:29:24 +0530

    added upload started and progress test in e2e

--- cypress/e2e/IKUpload.cy.js
+++ cypress/e2e/IKUpload.cy.js
@@ -6,16 +6,22 @@ describe('ik-upload component', () => {
   describe('Image Upload', () => {
       it('should upload image and render same image', () => { 
           // launch URL
-          cy.visit(APP_HOST);
+          cy.visit(APP_HOST,{
+            onBeforeLoad(win) {
+              cy.stub(win.console, 'log').as('consoleLog')
+            },
+          });
 
           //static file
           const p = 'sample.jpeg'
 
           // Upload file with cy.uploadFile
           cy.get('.file-upload-ik input[type="file"]').attachFile(p)
-
+          cy.wait(1000);
+          cy.get('@consoleLog').should('be.calledWith', 'Upload started')
           // wait for 2 secs
-          cy.wait(4000);
+          cy.wait(3000);
+          cy.get('@consoleLog').should('be.calledWith', 'Upload inprogress ... (100.00% Done)')
 
           //Verify uploaded file
           cy.get('.uploaded-img-ik').invoke('attr', 'src').should('contain','https://ik.imagekit.io/');
